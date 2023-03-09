ccec89691100215062ddc4f798a8680b31440473
Carmelo Campos <carmelogunsroses@gmail.com>
2023-03-09 15:19:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# Valorant Agent Instalock
+
+Este es un script para instalokear uwu
+
+
+
+[![Python 3.11.1](https://img.shields.io/badge/python-v3.11.1-blue.svg)](https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe)
+
+
+## Instalacion
+
+Debes tener instalado Python3, se recomienda usar 3.11.1
+
+```bash
+pip install -r requirements.txt
+```
+
+## Ejecutar
+
+Iniciar el programa instalocker
+
+```bash
+python instalock.py
+```
+
+
+## Screenshots
+
+![App Screenshot](https://i.imgur.com/Oj0JyDx.jpg)
+
+
+## Authors
+
+- [@ZEROv20](https://github.com/ZEROv20)
+- [@0xFFD](https://github.com/0xFFD)
+- [@CarmeloCampos](https://github.com/CarmeloCampos)
+
---
+++ instalock.py
@@ -0,0 +1,44 @@
+import json
+from requests import session
+from valclient.client import Client
+import time
+print('Valorant Agent instalocker by ZEROv20')
+print('Modificado por CarmeloCampos')
+
+try:
+    client = Client(region="latam")
+    client.activate()
+except Exception as e:
+    print('No tienes valorant abierto.')
+    exit()
+
+valid = False
+agents = {}
+seenMatches = []
+
+with open('data.json', 'r') as f:
+    agents = json.load(f)
+
+while valid == False:
+    try:
+        preferredAgent = input("Preferred Agent (e.g Gekko): ").lower()
+        if (preferredAgent in agents['agents'].keys()):
+            valid = True
+        else:
+            print("Invalid Agent")
+    except:
+        print("Input Error")
+
+print("Waiting for Agent Select")
+while True:
+    try:
+        sessionState = client.fetch_presence(client.puuid)['sessionLoopState']
+        if ((sessionState == "PREGAME") and (client.pregame_fetch_match()['ID'] not in seenMatches)):
+            print('Agent Select Found')
+            time.sleep(2)
+            client.pregame_select_character(agents['agents'][preferredAgent])
+            client.pregame_lock_character(agents['agents'][preferredAgent])
+            seenMatches.append(client.pregame_fetch_match()['ID'])
+            print('Successfully Locked ' + preferredAgent.capitalize())
+    except Exception as e:
+        print('', end='') # Using pass caused weird behavior
